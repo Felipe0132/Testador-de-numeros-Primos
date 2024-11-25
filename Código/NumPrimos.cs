ec962ea40e094e927b1e3a7c4b985f219b1b8f8c
Felipe Silva <felipesiilvaa2006@gmail.com>
2024-11-25 13:44:55 -0300

    Ajustes
    Ajustes para ReadLines e introdução de um GoTo

--- Código/NumPrimos.cs
+++ Código/NumPrimos.cs
@@ -7,6 +7,9 @@ class Código{
 
         Console.WriteLine("Esse programa vai mostrar para você se o número escolhido é primo ou não, vamos nessa?");
         resp1 = Console.ReadLine();
+
+        reinício:
+
         Console.WriteLine("Qual número você deseja testar? (Somente números inteiros)");
         numtest = int.Parse(Console.ReadLine());
         Console.WriteLine("Deseja que o programe que prove o porque do resultado?[Sim / Nao]");
@@ -14,13 +17,27 @@ class Código{
 
         switch(resp2){
             case "Não":
+            case "não":
+            case "nao":
+            case "Nao":
             if(numtest == 1 | numtest == 2){    
                 Console.WriteLine("O número {0} é um número primo pois so divide por ele mesmo e 1!", numtest);
             }else{
                 
             }
             case "Sim":
+            case "sim":
         
+        }
+
+        Console.WriteLine("Deseja testar outro número?");
+        resp2 = Console.ReadLine();
+
+        if(resp2 == "Não" || resp2 == "não" || resp2 == "nao" || resp2 == "Nao"){
+            Console.Clear();
+            goto reinício;
+        }else{
+            Console.WriteLine("Fim do programa, obrigado por testar!");
         }
     }
 }
